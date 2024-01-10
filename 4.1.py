c0b1df1f74015f11ceda5391284f71c1ef8724f8
gkocbo <156221821+gkocbo@users.noreply.github.com>
2024-01-10 20:01:59 +0300

    левая прога
    пробуем загрузить

---
+++ 4.1.py
@@ -0,0 +1,13 @@
+def gam(x, eps = 10**(-8)):
+    n = 1
+    pr = 0
+    bud = 1
+    while bud - pr > eps:
+        pr = bud
+        bud = pr * (((1 + (1/n))**x) / (1 + (x/n)))
+        n = n + 1
+    return pr/x
+
+
+
+print(gam(3), gam(3, 0.01), gam(x = 5, eps = 10**(-10)))
